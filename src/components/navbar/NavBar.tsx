6477c8fa9ea1dd40e70ce49fcf3752bb6db0ff50
Bonnie@WSL <ben.nicls@outlook.com>
2023-11-21 21:15:35 +0100

    Changed a few stuff in navbar stylesheet

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import './NavBar.css';
 
 function Navbar() {
   return (
-    <nav>
-      <div>
-        <a href="#title"><p>COMPANY</p></a>
-      </div>
-      <div>
-        <a href="#home"><i className="material-icons">home</i></a>
-        <a href="#account"><i className="material-icons">person</i></a>
-        <a href="#cart"><i className="material-icons">shopping_cart</i></a>
-      </div>
-	</nav>
+    <div className='navbar-container'>
+      <nav>
+        <div>
+          <a className="title" href="#title"><p>COMPANY</p></a>
+        </div>
+        <div className="navbar-right">
+          <a href="#account"><i className="material-icons">person</i></a>
+          <a href="#cart"><i className="material-icons">shopping_cart</i></a>
+        </div>
+	    </nav>
+    </div>
+
   );
 }
 
