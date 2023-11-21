a3df6a9b47f2b2f7f2d6baeb9a4b5e020bba7919
Bonnie@WSL <ben.nicls@outlook.com>
2023-11-21 20:08:07 +0100

    Link material icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './NavBar';
+import Navbar from './components/navbar/NavBar';
 
 function App() {
   return (
