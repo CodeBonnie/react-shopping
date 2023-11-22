3757ab79fb84fce94f7b8c677bae65c75fe5d6ed
Bonnie@WSL <ben.nicls@outlook.com>
2023-11-22 03:06:02 +0100

    Setup Router and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar/NavBar';
+
+import Shop from './pages/shop/Shop';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Shop />
+  },
+]);
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <RouterProvider router={router} />
     </div>
   );
 }
